5d5bd2a531ce40f840d717fa8d965bcd09949d63
HYEZ <oohyejioo@gmail.com>
2021-04-01 22:42:26 +0900

    upload models

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 data_root = '/Users/ohyeji/Desktop/연구실/dataset/kakao-arena-brunch/'
 data = '/Users/ohyeji/Desktop/연구실/대회_해커톤/kakao-arena-2nd/res/'
 root = '/Users/ohyeji/Desktop/연구실/대회_해커톤/kakao-arena-2nd/'
+submission = '/Users/ohyeji/Desktop/연구실/대회_해커톤/kakao-arena-2nd/submission/'
